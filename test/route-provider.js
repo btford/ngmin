b9dbd0174436bb65d6ad1de840aaffcb9d023693
Brian Ford <btford@umich.edu>
2013-07-01 17:23:42 -0700

    add chained router test

--- test/route-provider.js
+++ test/route-provider.js
@@ -46,4 +46,41 @@ describe('annotate', function () {
   });
 
 
+  it('should annotate chained $routeProvider.when()', function () {
+    var annotated = annotate(function () {
+      angular.module('myMod', []).
+        config(function ($routeProvider) {
+          $routeProvider.
+            when('path', {
+              controller: function ($scope) {
+                $scope.works = true;
+              }
+            }).
+            when('other/path', {
+              controller: function ($http) {
+                $http.get();
+              }
+            });
+        });
+    });
+
+    annotated.should.equal(stringifyFunctionBody(function () {
+      angular.module('myMod', []).
+        config(['$routeProvider', function ($routeProvider) {
+          $routeProvider.
+            when('path', {
+              controller: ['$scope', function ($scope) {
+                $scope.works = true;
+              }]
+            }).
+            when('other/path', {
+              controller: ['$http', function ($http) {
+                $http.get();
+              }]
+            });
+        }]);
+    }));
+  });
+
+
 });
