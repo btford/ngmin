15b34482e422032135376f9a48debf61aa364d7a
Brian Ford <btford@umich.edu>
2014-04-03 00:18:35 -0700

    chore: refactor out dependencies

--- test/util.js
+++ test/util.js
@@ -4,37 +4,11 @@
  */
 
 
-var esprima = require('esprima'),
-  escodegen = require('escodegen');
+var body = require('fn-body'),
+    normalize = require('normalize-fn');
 
-var fnDecl = /^[ ]*function[ ]?\(\)[ ]?\{\n/m,
-  trailingBrace = /[ ]*\}(?![\s\S]*\})/m;
-
-// given a function, return its body as a string.
+// given a function, return its body as a normalized string.
 // makes tests look a bit cleaner
 exports.stringifyFunctionBody = function (fn) {
-  var out = fn.toString().
-    replace(fnDecl, '').
-    replace(trailingBrace, '');
-
-  // then normalize with esprima/escodegen
-  var ast = esprima.parse(out, {
-    tolerant: true,
-    comment: true,
-    range: true,
-    tokens: true
-  });
-  // TODO: unstable API, see https://github.com/Constellation/escodegen/issues/10
-  ast = escodegen.attachComments(ast, ast.comments, ast.tokens);
-
-  out = escodegen.generate(ast, {
-    format: {
-      indent: {
-        style: '  '
-      }
-    },
-    comment: true
-  });
-
-  return out;
+  return normalize(body(fn));
 };
